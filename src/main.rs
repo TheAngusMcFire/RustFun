cbc73a0d9ad3398eeb2d213926882693c577ae4a
Christian Rieger <christian.rieger@student.tugraz.at>
2018-12-29 15:42:17 +0100

    added second file

--- src/main.rs
+++ src/main.rs
@@ -3,9 +3,13 @@
 extern crate regex;
 
 use regex::Regex;
+mod stuff;
+
 
 fn main() 
 {
+    stuff::test();
+
     let re = Regex::new(r"^\d{4}-\d{2}-\d{2}$").unwrap();
     println!("Did our date match? {}", re.is_match("2014-01-01"));
 }
