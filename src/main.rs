4cf2ac27c8133e59e4cfdedba5df650bcddecef8
Christian Rieger <christian.rieger@student.tugraz.at>
2018-12-29 15:50:47 +0100

    test

--- src/main.rs
+++ src/main.rs
@@ -1,14 +1,33 @@
 #![allow(non_snake_case)]
 
 extern crate regex;
+extern crate winit;
 
 use regex::Regex;
 mod stuff;
+
+fn showGui()
+{
+    let mut events_loop = winit::EventsLoop::new();
+    let window = winit::Window::new(&events_loop).unwrap();
+
+    events_loop.run_forever(|event| {
+        match event {
+            winit::Event::WindowEvent {
+              event: winit::WindowEvent::CloseRequested,
+              ..
+            } => winit::ControlFlow::Break,
+            _ => winit::ControlFlow::Continue,
+        }
+    });
+
+}
 
 
 fn main() 
 {
     stuff::test();
+    showGui();
 
     let re = Regex::new(r"^\d{4}-\d{2}-\d{2}$").unwrap();
     println!("Did our date match? {}", re.is_match("2014-01-01"));
